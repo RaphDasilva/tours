27c560f9aaaa4d6bad974fb1c961b0d9cc29e28a
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-03-20 20:53:28 +0530

    Add readmore function and remove tour function.

--- src/components/TourDisplay.js
+++ src/components/TourDisplay.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { tourActions } from '../redux/ToursSlice';
 
 const TourDisplay = ({
   id, name, image, info, price,
-}) => (
-  <article className="single-tour" key={id}>
-    <img src={image} alt={name} />
-    <footer>
-      <div className="tour-info">
-        <h4>{name}</h4>
-        <h4 className="tour-price">
-          $
-          {price}
-        </h4>
-      </div>
-      <p>
-        {info}
-      </p>
-      <button type="button" className="delete-btn">
-        not interested
-      </button>
-    </footer>
-  </article>
-);
+}) => {
+  const [readMore, setReadMore] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleRemove = (event) => {
+    const { id } = event.target.dataset;
+    dispatch(tourActions.remove(id));
+  };
+  return (
+    <article className="single-tour" key={id}>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">
+            $
+            {price}
+          </h4>
+        </div>
+        <p>
+          {
+            readMore ? info : `${info.substring(0, 200)}...`
+          }
+          <button type="button" onClick={() => setReadMore(!readMore)}>
+            {
+              readMore ? 'show less' : 'read more'
+            }
+          </button>
+        </p>
+        <button type="button" className="delete-btn" data-id={id} onClick={handleRemove}>
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
 
 TourDisplay.propTypes = {
   id: PropTypes.number.isRequired,
